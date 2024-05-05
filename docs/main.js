757d41bfa2af71b9a0621c74a10b977f980494e3
Henry <onkelhoy@gmail.com>
2024-05-05 15:59:43 +0200

    add: assets

--- docs/main.js
+++ docs/main.js
@@ -4,8 +4,22 @@ import {world} from "world";
 window.onload = () => {
   const btn = document.querySelector('button');
 
+  fetchTranslations();
+
   btn.onclick = () => {
     hello();
     world();
+  }
+}
+
+async function fetchTranslations() {
+  try {
+    const res = await fetch('public/translations/en.json');
+    const t = await res.json();
+
+    console.log('yes here is translations:', t['hello'], t);
+  }
+  catch (e) {
+    console.error('ERROR', e)
   }
 }
